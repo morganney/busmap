9448d391e636447de3f80f6e5bad3bb048a3f7b8
Morgan Ney <morganney@gmail.com>
2023-09-04 08:55:11 -0500

    build: export more components, error on lint warnings.

--- packages/ui/src/home.tsx
+++ packages/ui/src/home.tsx
@@ -1,8 +1,13 @@
 import styled from 'styled-components'
+import { Clear } from '@busmap/components/icons/clear'
 
 const Content = styled.p`
   font-weight: 700;
 `
 export const Home = () => {
-  return <Content>This is Home page content.</Content>
+  return (
+    <Content>
+      This is Home page content. <Clear />
+    </Content>
+  )
 }
