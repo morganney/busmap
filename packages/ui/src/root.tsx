428ea519ec88a1dba8e54e45cfb0106a3c851fe0
Morgan Ney <morganney@gmail.com>
2023-10-18 18:43:53 -0500

    feat: toasts. (#42)

--- packages/ui/src/root.tsx
+++ packages/ui/src/root.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router-dom'
+import { Toaster } from '@busmap/components/toast'
 
 import { Layout } from './layout.js'
 import { ErrorBoundary } from './components/errorBoundary.js'
 
+// TODO: Should fetch agencies here and set it in context
 const Root = () => {
   return (
     <ErrorBoundary>
+      <Toaster anchor="top left" />
       <Layout>
         <Outlet />
       </Layout>
