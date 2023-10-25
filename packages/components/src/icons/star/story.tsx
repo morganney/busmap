96866b160f2580626ddc51140a6055eac4efdae5
Morgan Ney <morganney@gmail.com>
2023-10-25 14:52:46 -0500

    chore: fontawesome integration.

---
+++ packages/components/src/icons/star/story.tsx
@@ -0,0 +1,31 @@
+import { Star } from './mod.js'
+
+import type { StoryFn } from '@storybook/react'
+
+const Primary: StoryFn<typeof Star> = args => {
+  return <Star {...args} />
+}
+
+export default {
+  title: 'Icons/Star',
+  component: Star,
+  args: {
+    outlined: false,
+    size: 'medium',
+    color: '#c1c1c1',
+    tabIndex: 0
+  },
+  argTypes: {
+    size: {
+      control: 'select',
+      options: ['small', 'medium', 'large']
+    },
+    color: {
+      control: 'color'
+    },
+    onClick: {
+      action: 'onClick'
+    }
+  }
+}
+export { Primary }
