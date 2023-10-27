880cee79cde488ae4cc90f70d867f45750948b1e
Morgan Ney <morganney@gmail.com>
2023-10-27 09:46:26 -0500

    chore: set up favorites worker. (#58)

--- packages/ui/src/components/favoriteStop.tsx
+++ packages/ui/src/components/favoriteStop.tsx
@@ -1,15 +1,14 @@
 import styled from 'styled-components'
+import { useCallback, useMemo } from 'react'
 import { Star } from '@busmap/components/icons/star'
 import { Tooltip } from '@busmap/components/tooltip'
 import { SY30T } from '@busmap/components/colors'
 
-import type { FC } from 'react'
-import type { Stop } from '../types.js'
+import { useGlobals } from '../globals.js'
+import { useStorage, useStorageDispatch } from '../contexts/storage.js'
 
-interface FavoriteStopProps {
-  stop?: Stop
-  favorite: boolean
-}
+import type { FC } from 'react'
+import type { Favorite } from '../contexts/storage.js'
 
 const Tip = styled(Tooltip)`
   display: flex;
@@ -20,12 +19,43 @@ const Button = styled.button`
   margin: 0;
   background: none;
 `
-const FavoriteStop: FC<FavoriteStopProps> = ({ stop, favorite = false }) => {
+const worker = new Worker(new URL('../workers/favorites.ts', import.meta.url), {
+  type: 'module'
+})
+const FavoriteStop: FC = () => {
+  const storage = useStorage()
+  const storageDispatch = useStorageDispatch()
+  const { agency, route, direction, stop } = useGlobals()
+  const favorite = useMemo(() => {
+    return storage.favorites?.find(fav => {
+      return (
+        `${fav.route.id}${fav.direction.id}${fav.stop.id}` ===
+        `${route?.id}${direction?.id}${stop?.id}`
+      )
+    })
+  }, [storage.favorites, stop, route, direction])
+  const onClick = useCallback(() => {
+    if (favorite) {
+      storageDispatch({ type: 'favoriteRemove', value: favorite })
+      worker.postMessage({ action: 'stop', favorite })
+    } else if (agency && route && direction && stop) {
+      const add: Favorite = {
+        stop: stop,
+        agency: agency,
+        route: { id: route.id, title: route.title ?? route.shortTitle },
+        direction: { id: direction.id, title: direction.title ?? direction.shortTitle }
+      }
+
+      storageDispatch({ type: 'favoriteAdd', value: add })
+      worker.postMessage({ action: 'start', favorite: add })
+    }
+  }, [storageDispatch, agency, route, direction, stop, favorite])
+
   if (stop) {
     return (
-      <Tip title={favorite ? 'Favorite stop.' : 'Add to favorites.'}>
+      <Tip title={favorite ? 'Remove favorite.' : 'Add favorite.'}>
         <Button>
-          <Star size="small" color={SY30T} outlined={!favorite} />
+          <Star size="small" color={SY30T} outlined={!favorite} onClick={onClick} />
         </Button>
       </Tip>
     )
