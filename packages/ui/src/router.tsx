7242e268617c5055a2c5229c72ebd32239ff076f
Morgan Ney <morganney@gmail.com>
2023-11-12 10:38:02 -0600

    refactor: add new route to better deselect stop.

--- packages/ui/src/router.tsx
+++ packages/ui/src/router.tsx
@@ -17,6 +17,11 @@ const router = createBrowserRouter([
             id: 'home-stop',
             path: 'stop/:agency/:route/:direction/:stop',
             element: <Home />
+          },
+          {
+            id: 'home-direction',
+            path: 'stop/:agency/:route/:direction',
+            element: <Home />
           }
         ]
       }
