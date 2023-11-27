b2211f24c8e947676f5bafdc5baa27e5b147343b
Morgan Ney <morganney@gmail.com>
2023-11-27 09:08:06 -0600

    refactor: whitespace.

--- initdb.d/insert.sh
+++ initdb.d/insert.sh
@@ -4,5 +4,5 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
   INSERT INTO "sso_provider" ("name", "client_id", "client_secret") VALUES
-  ('Google OAuth', '$SSO_GOOG_CLIENT_ID',	'$SSO_GOOG_CLIENT_SECRET');
+  ('Google OAuth', '$SSO_GOOG_CLIENT_ID', '$SSO_GOOG_CLIENT_SECRET');
 EOSQL
