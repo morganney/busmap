3dba4cd7ad5991b1875357d65388c4fb8cc3c504
Morgan Ney <morganney@gmail.com>
2023-12-04 20:36:27 -0600

    chore: update db settings. resolves #100 (#125)
    closes #100

--- packages/api/src/db.ts
+++ packages/api/src/db.ts
@@ -5,6 +5,16 @@ import postgres from 'postgres'
 const sql = postgres({
   host: 'db',
   port: 5432,
+  /**
+   * Sets db connections.
+   * @see https://github.com/porsager/postgres/issues/369#issuecomment-1129580233
+   */
+  max: 5,
+  /**
+   * Not sure if the same as what is in this doc.
+   * @see https://www.postgresql.org/docs/current/runtime-config-client.html#GUC-IDLE-SESSION-TIMEOUT
+   */
+  idle_timeout: 0,
   database: env.POSTGRES_DB,
   username: env.POSTGRES_USER,
   password: env.POSTGRES_PASSWORD
