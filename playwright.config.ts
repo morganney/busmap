d5b646f344f03fe677e48ed59694848574d2c5c0
Morgan Ney <morganney@gmail.com>
2024-01-14 14:11:05 -0600

    ci: updates.

--- playwright.config.ts
+++ playwright.config.ts
@@ -58,7 +58,7 @@ export default defineConfig({
     command: 'docker compose up --attach-dependencies dev',
     url: 'http://localhost:3000/health',
     reuseExistingServer: !process.env.CI,
-    timeout: 60_000 * 5,
+    timeout: 60_000 * 7,
     stdout: 'pipe'
   }
 })
