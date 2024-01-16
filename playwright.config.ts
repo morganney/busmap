e586bf10cbc9e823e989cf21d9d154b6ed9cb639
Morgan Ney <morganney@gmail.com>
2024-01-16 10:00:51 -0600

    ci: logs.

--- playwright.config.ts
+++ playwright.config.ts
@@ -59,6 +59,7 @@ export default defineConfig({
     ignoreHTTPSErrors: true,
     url: 'https://localhost/healthcheck',
     reuseExistingServer: !process.env.CI,
-    timeout: 60_000 * 3
+    timeout: 60_000 * 5,
+    stdout: 'pipe'
   }
 })
