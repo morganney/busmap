e00d4c6115091b35cbcbd0b5658c2c82a823911b
Morgan Ney <morganney@gmail.com>
2024-01-18 09:39:29 -0600

    test: simplify projects config.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig, devices } from '@playwright/test'
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
@@ -18,7 +12,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -31,54 +25,40 @@ export default defineConfig({
   },
 
   /* Configure projects for major browsers */
-  projects: process.env.CI
-    ? [
-        /**
-         * Ubuntu and mkcert issues with Chrome / Firefox
-         * @see https://github.com/FiloSottile/mkcert/issues/447
-         *
-         * Other option is to use macos GitHub action runner and
-         * install docker, etc. Could also, use `ignoreHTTPSErrors`
-         * from playwright in the test specs to cover more browsers.
-         */
-        {
-          name: 'webkit',
-          use: { ...devices['Desktop Safari'] }
-        },
-        {
-          name: 'chromium',
-          use: {
-            ...devices['Desktop Chrome'],
-            ignoreHTTPSErrors: true
-          },
-        },
-        /* Test against mobile viewports. */
-        {
-          name: 'Mobile Chrome',
-          use: {
-            ...devices['Pixel 5'],
-            ignoreHTTPSErrors: true
-          },
-        },
-        {
-          name: 'Mobile Safari',
-          use: { ...devices['iPhone 12'] }
-        }
-      ]
-    : [
-        {
-          name: 'chromium',
-          use: { ...devices['Desktop Chrome'] }
-        },
-        {
-          name: 'webkit',
-          use: { ...devices['Desktop Safari'] }
-        },
-        {
-          name: 'Mobile Safari',
-          use: { ...devices['iPhone 12'] }
-        }
-      ],
+  projects: [
+    /**
+     * Ubuntu and mkcert issues with Chrome / Firefox
+     * @see https://github.com/FiloSottile/mkcert/issues/447
+     *
+     * Other option is to use macos GitHub action runner and
+     * install docker, etc. Could also, use `ignoreHTTPSErrors`
+     * from playwright in the test specs to cover more browsers.
+     */
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] }
+    },
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        ignoreHTTPSErrors: Boolean(process.env.CI)
+      }
+    },
+
+    /* Test against mobile viewports. */
+    {
+      name: 'Mobile Chrome',
+      use: {
+        ...devices['Pixel 5'],
+        ignoreHTTPSErrors: Boolean(process.env.CI)
+      }
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] }
+    }
+  ],
 
   /* Run your local dev server before starting the tests */
   webServer: process.env.TEST_BASE_URL
