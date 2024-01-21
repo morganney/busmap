b48dae4b1bfdfb998356bb74816a7f7674b7a6ca
Morgan Ney <morganney@gmail.com>
2024-01-20 18:10:57 -0600

    feat: touch event to close flyout.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,48 @@
+import { env } from 'node:process'
+
 import { defineConfig, devices } from '@playwright/test'
 
+const { CI, TEST_BASE_URL } = env
+const ciBrowsers = [
+  {
+    name: 'chromium',
+    use: {
+      ...devices['Desktop Chrome'],
+      ignoreHTTPSErrors: Boolean(CI)
+    }
+  },
+  {
+    name: 'Mobile Safari',
+    use: { ...devices['iPhone 12'] }
+  }
+]
+const devBrowsers = [
+  ...ciBrowsers,
+  /**
+   * Ubuntu and mkcert issues with Chrome / Firefox
+   * @see https://github.com/FiloSottile/mkcert/issues/447
+   *
+   * Other option is to use macos GitHub action runner and
+   * install docker, etc. Could also, use `ignoreHTTPSErrors`
+   * from playwright in the test specs to cover more browsers.
+   */
+  {
+    name: 'webkit',
+    use: { ...devices['Desktop Safari'] }
+  },
+  {
+    name: 'Mobile Chrome',
+    use: {
+      ...devices['Pixel 5'],
+      ignoreHTTPSErrors: Boolean(CI)
+    }
+  }
+]
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
-  retries: process.env.CI ? 1 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  retries: CI ? 1 : 0,
+  workers: CI ? 1 : undefined,
   reporter: 'list',
   /**
    * Shared settings for all the projects below.
@@ -12,50 +50,19 @@ export default defineConfig({
    */
   use: {
     // Base URL to use in actions like `await page.goto('/')`.
-    baseURL: process.env.TEST_BASE_URL ?? 'https://localhost',
+    baseURL: TEST_BASE_URL ?? 'https://localhost',
     trace: 'on-first-retry'
   },
-  projects: [
-    /**
-     * Ubuntu and mkcert issues with Chrome / Firefox
-     * @see https://github.com/FiloSottile/mkcert/issues/447
-     *
-     * Other option is to use macos GitHub action runner and
-     * install docker, etc. Could also, use `ignoreHTTPSErrors`
-     * from playwright in the test specs to cover more browsers.
-     */
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] }
-    },
-    {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-        ignoreHTTPSErrors: Boolean(process.env.CI)
-      }
-    },
-    {
-      name: 'Mobile Chrome',
-      use: {
-        ...devices['Pixel 5'],
-        ignoreHTTPSErrors: Boolean(process.env.CI)
-      }
-    },
-    {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] }
-    }
-  ],
-  webServer: process.env.TEST_BASE_URL
+  projects: CI ? ciBrowsers : devBrowsers,
+  webServer: TEST_BASE_URL
     ? undefined
     : {
-        command: process.env.CI
+        command: CI
           ? 'docker compose -f compose.yaml up --attach-dependencies stage'
           : 'docker compose up --attach-dependencies dev',
         ignoreHTTPSErrors: true,
         url: 'https://localhost/healthcheck',
-        reuseExistingServer: !process.env.CI,
+        reuseExistingServer: !CI,
         timeout: 60_000 * 7
       }
 })
