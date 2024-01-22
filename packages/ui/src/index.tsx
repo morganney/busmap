59c3863977de9e3b069bfc3005ff314c14f6838a
Morgan Ney <morganney@gmail.com>
2024-01-22 14:42:37 -0600

    fix: font cache headers, leaflet marker img. (#191)

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { Icon } from 'leaflet'
 
 import { router } from './router.js'
 
@@ -7,5 +8,12 @@ import 'leaflet/dist/leaflet.css'
 
 const rootEl = document.querySelector('main') as HTMLElement
 const root = createRoot(rootEl)
+
+Icon.Default.prototype.options.iconUrl =
+  'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png'
+Icon.Default.prototype.options.iconRetinaUrl =
+  'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png'
+Icon.Default.prototype.options.shadowUrl =
+  'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png'
 
 root.render(<RouterProvider router={router} />)
